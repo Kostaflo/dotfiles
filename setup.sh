55643e068040ff7b59c2b7611705a34db7ac67d0
KostafloLinux <kostaflo72@gmail.com>
2025-05-27 21:43:52 +0300

    added a monitor and updated setup script

--- setup.sh
+++ setup.sh
@@ -3,11 +3,24 @@
 set -e
 
 # Main CLI tools
-sudo pacman -Syu  --needed --noconfirm \
-	zsh neovim tmux fzf bat starship zoxide tldr stow
+echo "Installing main CLI tools..."
+sudo pacman -Syu  --needed --noconfirm zsh neovim tmux fzf bat starship zoxide tldr stow
 
 # Wayland-specific tools
-sudo pacman -Syu  --needed --noconfirm \
-	wayabar wofi
+sudo pacman -Syu  --needed --noconfirm wayabar wofi
 
+if [ ! -f ".stow-local-ignore" ]; then
+	git clone https://github.com/Kostaflo/dotfiles.git ~/dotfiles
+	cd ~/dotfiles
+fi
 
+for dir in */; do
+	if [ -d "$dir" ]; then
+		echo "Stowing $dir..."
+		stow "$dir"
+	fi
+done
+
+chsh -s "$(which zsh)"
+
+echo "Setup complete! Please log out and log in again to use zsh."
