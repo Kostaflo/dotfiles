620243b4cfe6c122a7a30c55dd5db51ade612339
KostafloLinux <kostaflo72@gmail.com>
2026-09-05 21:34:35 +0300

    Changed the setup script

--- setup.sh
+++ setup.sh
@@ -4,24 +4,35 @@ set -e
 
 # Main CLI tools
 echo "Installing main CLI tools..."
-sudo pacman -Syu  --needed --noconfirm zsh neovim tmux fzf bat starship zoxide tldr stow
+sudo pacman -Syu --needed --noconfirm zsh neovim tmux fzf bat starship zoxide tldr stow ghostty kitty
 
-# Wayland-specific tools
+# Wayland-specific tools (hyprshot is in the extra repo)
 echo "Installing Wayland-specific tools..."
-sudo pacman -Syu  --needed --noconfirm waybar wofi hyprpaper hyprshot hyprlock swaync rofi
+sudo pacman -S --needed --noconfirm waybar wofi hyprpaper hyprshot hyprlock swaync rofi
 
-if [ ! -f ".stow-local-ignore" ]; then
-	git clone https://github.com/Kostaflo/dotfiles.git ~/dotfiles
-	cd ~/dotfiles
+# Clone & Setup dotfiles
+if [ ! -d "$HOME/dotfiles" ]; then
+    echo "Cloning dotfiles repository..."
+    git clone https://github.com/Kostaflo/dotfiles.git "$HOME/dotfiles"
+else
+    echo "Dotfiles repository already exists in $HOME/dotfiles"
 fi
 
+cd "$HOME/dotfiles"
+
+# Stow each directory safely to the home folder
 for dir in */; do
-	if [ -d "$dir" ]; then
-		echo "Stowing $dir..."
-		stow "$dir"
-	fi
+    if [ -d "$dir" ]; then
+        target_dir="${dir%/}"
+        echo "Stowing $target_dir..."
+        stow -t "$HOME" "$target_dir"
+    fi
 done
 
-chsh -s "$(which zsh)"
+# Set default shell to zsh
+if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Changing default shell to zsh..."
+    chsh -s "$(which zsh)"
+fi
 
 echo "Setup complete! Please log out and log in again to use zsh."
